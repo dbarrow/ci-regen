3a1dff68204829de4adfea5f7caf17aed6ff071d
dbarrow <darryl@traversepoint.com>
2013-02-23 15:35:25 -0500

    update html

--- application/core_modules/install/views/complete.php
+++ application/core_modules/install/views/complete.php
@@ -1,7 +1,7 @@
 <?php 
 	echo($db_installed && $tables_installed) ? "<h1>Install Complete!</h1>"  : "<h1>Installation Error</h1>";
-	echo($db_installed)                      ? "DB Config successfully installed" : "Database config installation error ";
-	echo($tables_installed)                  ? "Database tables successfully installed"   : "Table Installation error";
+	echo($db_installed)                      ? "<p>DB Config successfully installed</p>" : "<p>Database config installation error </p>";
+	echo($tables_installed)                  ? "<p>Database tables successfully installed</p>"   : "<p>Table Installation error</p>";
 
 	if($db_installed && $tables_installed) 
 	{
