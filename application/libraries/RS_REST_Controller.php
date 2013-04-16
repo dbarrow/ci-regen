8259a7141e758da09e3e9ff691a245f704ab6002
dbarrow <darryl@traversepoint.com>
2013-04-15 21:12:06 -0400

    testing headers

--- application/libraries/RS_REST_Controller.php
+++ application/libraries/RS_REST_Controller.php
@@ -47,7 +47,7 @@ class RS_REST_Controller extends REST_Controller
 		$service_name = explode( '_', $this->model);
        	$auth = $this->services_model->get_by('name', $service_name[0]);
 
-       	if( $_SERVER['HTTP_REQUEST_METHOD'] == 'OPTIONS')
+       	if( $_SERVER['REQUEST_METHOD'] == 'OPTIONS')
 		{
 			$this->response(200);  //Not authorized
 		}
@@ -59,7 +59,7 @@ class RS_REST_Controller extends REST_Controller
 
 			if (!$this->api_authorization->authorize_token($token)) 
 			{
-    			$this->response(array('status' => $_SERVER['HTTP_REQUEST_METHOD'], 'error' => 'Not Authorized - Login in'), 401);  //Not authorized
+    			$this->response(array('status' => $_SERVER, 'error' => 'Not Authorized - Login in'), 401);  //Not authorized
     		}	
     		
 	    	$new_token = time();
