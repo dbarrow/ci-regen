55a8059f8c85e000097475bcb34355e800e9ece0
dbarrow <darryl@traversepoint.com>
2013-04-15 20:53:09 -0400

    testing headers

--- application/libraries/RS_REST_Controller.php
+++ application/libraries/RS_REST_Controller.php
@@ -59,7 +59,7 @@ class RS_REST_Controller extends REST_Controller
 
 			if (!$this->api_authorization->authorize_token($token)) 
 			{
-    			$this->response(array('status' => $token, 'error' => 'Not Authorized - Login in'), 401);  //Not authorized
+    			$this->response(array('status' => $_SERVER['HTTP_OPTIONS'], 'error' => 'Not Authorized - Login in'), 401);  //Not authorized
     		}	
     		
 	    	$new_token = time();
