c4adea1dd25378b66cd209f32ddc21fe2e6beacd
dbarrow <darryl@traversepoint.com>
2013-05-19 21:03:00 -0400

    update urls

--- frontend/modules/projects/config.js
+++ frontend/modules/projects/config.js
@@ -1,10 +1,10 @@
 
 myApp.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
 
-    $routeProvider.when('/projects', {templateUrl: 'modules/projects/views/projects.html', controller: ProjectListController});
-    $routeProvider.when('/project/create', {templateUrl: 'modules/projects/views/new-project.html' ,controller: ProjectListController});
-    $routeProvider.when('/project/:id', {templateUrl: 'modules/projects/views/project.html' ,controller: ProjectViewController});
-    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: LoginController});
+    $routeProvider.when('/projects', {templateUrl: 'frontend/modules/projects/views/projects.html', controller: ProjectListController});
+    $routeProvider.when('/project/create', {templateUrl: 'frontend/modules/projects/views/new-project.html' ,controller: ProjectListController});
+    $routeProvider.when('/project/:id', {templateUrl: 'frontend/modules/projects/views/project.html' ,controller: ProjectViewController});
+    $routeProvider.when('/login', {templateUrl: 'frontend/partials/login.html', controller: LoginController});
 
     $routeProvider.otherwise({redirectTo: 'projects'});
   }]);
